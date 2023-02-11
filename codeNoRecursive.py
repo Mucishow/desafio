40f06e8aa54639d501fd50dc484c372f7fabb548
Massilon <massilon.lourenco@premium-minds.com>
2023-02-11 23:33:43 +0000

    Add time limit instead of operations limit

--- codeNoRecursive.py
+++ codeNoRecursive.py
@@ -1,5 +1,5 @@
 import sys
-
+import time
 #REMOVE RECURSIVE AND TEST OTHER LIMITS
 maxValue = 0
 flow = []
@@ -8,13 +8,12 @@ maxInputChain = 0
 inputSize = 0
 operations = 0
 	
-def recursive(a, dictio, chain):
+def recursive(a, dictio, chain, startTime):
 	global maxValue
 	global maxFlow
 	global maxInputChain
 	global inputSize
 	global operations
-	
 	memoryStack = []
 
 	edges = list(filter(lambda posib: dictio[a][posib][0] > 0, dictio[a].keys()))
@@ -22,6 +21,8 @@ def recursive(a, dictio, chain):
 	memoryStack.append([edges, 0, a, 0])
 	while(len(memoryStack) > 0):
 		edges, i, a, chain = memoryStack.pop()
+		if(time.time() - startTime > 29.1):
+			return len(maxFlow)
 		flag = False
 		while i < len(edges):
 			possible = edges[i]
@@ -40,8 +41,6 @@ def recursive(a, dictio, chain):
 				if(chain + 1 > maxValue):
 					maxValue = chain + 1
 					maxFlow = flow.copy()
-				if(chain + 1 >= maxInputChain):
-					return maxInputChain
 				if(not dictio):
 					return len(maxFlow)
 
@@ -55,30 +54,23 @@ def recursive(a, dictio, chain):
 				if(chain + 1> maxValue):
 					maxValue = chain + 1
 					maxFlow = flow.copy()
-				if(chain + 1>= maxInputChain):
-					return maxInputChain
 				if(not dictio):
 					return len(maxFlow)
 
 				break
 		if(not flag):
-			if(inputSize > 45):
-				operations = operations + 1 
-			if(operations == 10):
-				operations = 0
-				maxInputChain = maxInputChain - 1
 			if(len(memoryStack) > 0):
 				possible = flow[-1]
 				dictio[flow[-2]][flow[-1]][0] = dictio[flow[-2]][flow[-1]][0] + 1
 				possible = flow.pop()
 	return -1
 
-def solve(a):
+def solve(a, startTime):
 	valor = 0
 	for value in sorted(list(a.keys())):
 		flow.append(value)
 		#print(a)
-		aux = recursive(value, a , 0)
+		aux = recursive(value, a , 0, startTime)
 		if(aux != -1):
 			return
 		if(maxValue > valor):
@@ -88,6 +80,7 @@ def solve(a):
 dict = {}
 inputSize = 0;
 
+startTime = time.time()
 for line in sys.stdin.readlines():
 	firstLetter = line[0].lower()
 	lastLetter = line[-2].lower()
@@ -105,7 +98,7 @@ if(inputSize == 0):
 
 maxInputChain = inputSize
 	
-solve(dict) 
+solve(dict, startTime) 
 count = 0;
 initial = maxFlow[0]
 for a in maxFlow:
